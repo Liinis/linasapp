5e1a6d52ab6d20c07af8593ff2897fb11009a89a
Lina Åkesson <linakesson@gmail.com>
2019-04-30 12:00:30 +0200

    fixat grunden i appen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Nyhetslista from './components/Nyhetslista';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Nyhetslista/>
+
+    /*<section>
+      <img src="https://source.unsplash.com/random/75*25/?music"></img>
+      <article></article>
+      <h2>Musiknyhet</h2>
+      <p>Här kommer en musiknyhet</p>
+      <a>Läs mer</a>
+   </section>
+   */
+
+    
   );
 }
 
---
+++ src/components/Nyhetsartikel.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Nyhetsartikel (){
+    return (
+        <article>
+            <img src="https://source.unsplash.com/random/75*25/?music"></img>
+            <h2>Musiknyhet</h2>
+            <p>Här kommer en musiknyhet</p>
+            <a>Läs mer</a>
+        </article>
+    );
+    }
+
+
+export default Nyhetsartikel
